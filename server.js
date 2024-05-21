7f5b92ab47723f86c70d0be1e1db27c20d48f3cb
dvicampos <dvicamp@gmail.com>
2024-05-21 11:23:04 -0600

    nuevas vistas gastos

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,7 +21,17 @@ const UserSchema = new mongoose.Schema({
     autorizacion: String
 });
 
+const gastoSchema = new mongoose.Schema({
+    monto: String,
+    gastos: String,
+    tipo: String,
+    tipoMonto: String,
+    comentarios: String,
+});
+
+
 const User = mongoose.model('User', UserSchema);
+const Gasto = mongoose.model('Gasto', gastoSchema);
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -41,10 +49,6 @@ app.post('/register', async (req, res) => {
     const { username, password } = req.body;
 
     try {
-        const existeuser = await User.findOne({username})
-        if(existeuser){
-            return res.status(400).send('El nombre de usuario ya esya en uso')
-        }
         const hashedPassword = await bcrypt.hash(password, 10);
         await User.create({ username, password: hashedPassword });
         res.redirect('/');
@@ -60,9 +64,6 @@ app.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ username });
         if (user && await bcrypt.compare(password, user.password)) {
-            const token = jwt.sign({ userId: user._id }, 'secreto', { expiresIn: '1h' });
-            // res.json({ token });
-            console.log(token)
             res.redirect('/users');
         } else {
             res.send('Credenciales inválidas');
@@ -97,6 +98,16 @@ app.get('/users', async (req, res) => {
         res.status(500).send('Error al obtener los usuarios');
     }
 });
+// Ruta para mostrar la lista de gastos
+app.get('/gastos', async (req, res) => {
+    try {
+        const gastos = await Gasto.find();
+        res.render('gastos', { gastos });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error al obtener los gastos');
+    }
+});
 
 // Leer un usuario por su ID
 app.get('/users/:id', async (req, res) => {
@@ -113,6 +124,21 @@ app.get('/users/:id', async (req, res) => {
         res.status(500).send('Error al obtener el usuario');
     }
 });
+// Leer un gasto por su ID
+app.get('/gastos/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const gasto = await Gasto.findById(id);
+        if (!gasto) {
+            return res.status(404).send('Gasto no encontrado');
+        }
+        res.send(gasto);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error al obtener el gasto');
+    }
+});
 
 // Middleware para editar un usuario
 app.get('/users/:id/edit', async (req, res) => {
@@ -126,6 +152,20 @@ app.get('/users/:id/edit', async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).send('Error al buscar el usuario');
+    }
+});
+// Middleware para editar un usuario
+app.get('/gastos/:id/edit', async (req, res) => {
+    const gastoId = req.params.id;
+    try {
+        const gasto = await Gasto.findById(gastoId);
+        if (!gasto) {
+            return res.status(404).send('Gasto no encontrado');
+        }
+        res.render('editGasto', { gasto });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error al buscar el gasto');
     }
 });
 
@@ -170,6 +210,49 @@ app.post('/users/:id/edit', async (req, res) => {
         res.status(500).send('Error al actualizar el usuario');
     }
 });
+app.post('/gastos/:id/edit', async (req, res) => {
+    const gastoId = req.params.id;
+    const { monto, gastos, tipo, tipoMonto, comentarios } = req.body;
+
+    try {
+        // Obtener el usuario existente de la base de datos
+        const gasto = await Gasto.findById(gastoId);
+        if (!gasto) {
+            return res.status(404).send('Gasto no encontrado');
+        }
+
+        // Construir el objeto con los datos actualizados
+        const updatedData = {};
+        if (monto) {
+            updatedData.monto = monto;
+        }
+        if (gastos) {
+            // Encriptar la nueva contraseña si se proporciona
+            updatedData.gastos = gastos
+        }
+        if (tipo) {
+            updatedData.tipo = tipo;
+        }
+        if (tipoMonto) {
+            updatedData.tipoMonto = tipoMonto;
+        }
+        if (comentarios) {
+            updatedData.comentarios = comentarios;
+        }
+
+        // Actualizar el usuario con los datos actualizados
+        const updatedGasto = await Gasto.findByIdAndUpdate(gastoId, updatedData, { new: true });
+        if (!updatedGasto) {
+            return res.status(404).send('Gasto no encontrado');
+        }
+
+        // Redireccionar a la página de lista de usuarios después de la actualización
+        res.redirect('/gastos');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error al actualizar el gastos');
+    }
+});
 
 
 
@@ -187,6 +270,20 @@ app.post('/users/:id/delete', async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).send('Error al eliminar el usuario');
+    }
+});
+// Eliminar un gastos
+app.post('/gastos/:id/delete', async (req, res) => {
+    const gastoId = req.params.id;
+    try {
+        const deletedGastos = await Gasto.findByIdAndDelete(gastoId);
+        if (!deletedGastos) {
+            return res.status(404).send('Gasto no encontrado');
+        }
+        res.redirect('/gastos');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error al eliminar el gastos');
     }
 });
 
